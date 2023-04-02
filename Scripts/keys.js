6f188c09b531a2d108f397a6ab4c715e70d72016
livinginthesunlight72@protonmail.com <livinginthesunlight72@protonmail.com>
2023-04-02 17:58:34 -0500

    Inicio

---
+++ Scripts/keys.js
@@ -0,0 +1,45 @@
+﻿
+var keys = {
+	bind : function() {
+	  
+	},
+	reset : function() {
+		keys.left = false;
+		keys.right = false;
+		keys.accelerate = false;
+		keys.up = false;
+		keys.down = false;
+	},
+	unbind : function() {
+		$(document).off('keydown');
+		$(document).off('keyup');
+	},
+	handler : function(event, status) {
+		
+		event.preventDefault();
+		return false;
+	},
+	accelerate : false,
+	left : false,
+	up : false,
+	right : false,
+	down : false,
+};
+$("#up").on("touchstart", function(){
+  keys.up = true
+  setTimeout(function() {
+    keys.up = 0
+  }, 500)
+})
+$("#right").on("touchstart", function(){
+  keys.right = true
+})
+$("#right").on("touchend", function() {
+  keys.right = false
+})
+$("#left").on("touchstart", function() {
+  keys.left = true
+})
+$("#left").on("touchend", function() {
+  keys.left = false
+})
